0036711b351acb747aa0072e6fdbdfe670260f1e
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-03-06 22:49:39 -0300

    configure role arn

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get("/health", async (request, reply) => {
-    return reply.status(200).send({ message: "OK" });
+    return reply.status(200).send({ message: "OK ✅" });
   });
 }
